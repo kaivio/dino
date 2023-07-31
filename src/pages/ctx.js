95a3ed7f08ffaca7b29e8c32b907c0edc32f6c51
kaivio <kaivio@github.com>
2023-07-31 20:12:20 +0800

    嵌入其他页面

---
+++ src/pages/ctx.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import { useDocsContext } from '@docusaurus/useGlobalData';
+
+import Page from '@site/src/pages/edit'
+import Page2 from '@site/docs/intro.md'
+
+function MyCustomPage() {
+   return (
+     <Layout>
+       <h1>一个普通页面</h1>
+       <Page />
+       <Page2 />
+     </Layout>
+   );
+}
+
+export default MyCustomPage;
