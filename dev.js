4282ceb0f0f09a92c74232b00d1e1e4f08fa2e4a
kaivio <kaivio@github.com>
2023-07-31 09:21:17 +0800

    修复windows平台报错 spawn npm ENOENT

--- dev.js
+++ dev.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 // 启动第一个子进程
-const childProcess1 = spawn('npm', [
+const childProcess1 = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', [
   'run', 'start', '--',
   '--host', '0.0.0.0',
   '--port', '3001',
@@ -28,6 +28,17 @@ childProcess2.stdout.on('data', (data) => {
 childProcess2.stderr.on('data', (data) => {
   console.error(`---  node server.js ---\n${data}`);
 });
+
+childProcess1.on('error', (err) => {
+  console.error(`--- ERROR npm run start  ---\n${err.message}`);
+  console.log(err)
+});
+childProcess2.on('error', (err) => {
+  console.error(`--- ERROR node server.js  ---\n${err.message}`);
+  console.log(err)
+
+});
+
 
 // 监听中断信号（如 Ctrl+C）
 process.on('SIGINT', () => {
