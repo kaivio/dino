a8e23f5b987d88eb7ebc740319c64d468cce3d3a
kaivio <kaivio@github.com>
2023-08-24 09:28:35 +0800

    调整启动器

--- dev.js
+++ dev.js
@@ -18,36 +18,46 @@ childProcess1.stderr.on('data', (data) => {
 });
 
 // 启动第二个子进程
-const childProcess2 = spawn('node', ['server.js']);
 
-// 绑定标准输出流和错误流
-childProcess2.stdout.on('data', (data) => {
-  console.log(`---  node server.js ---\n${data}`);
-});
+let childProcess2
+function start(params) {
+  childProcess2 = spawn('node', ['server.js']);
 
-childProcess2.stderr.on('data', (data) => {
-  console.error(`---  node server.js ---\n${data}`);
-});
+  // 绑定标准输出流和错误流
+  childProcess2.stdout.on('data', (data) => {
+    console.log(`---  node server.js ---\n${data}`);
+  });
 
-childProcess1.on('error', (err) => {
-  console.error(`--- ERROR npm run start  ---\n${err.message}`);
-  console.log(err)
-});
-childProcess2.on('error', (err) => {
-  console.error(`--- ERROR node server.js  ---\n${err.message}`);
-  console.log(err)
+  childProcess2.stderr.on('data', (data) => {
+    console.error(`---  node server.js ---\n${data}`);
+  });
 
-});
+  childProcess1.on('error', (err) => {
+    console.error(`--- ERROR npm run start  ---\n${err.message}`);
+    console.log(err)
+  });
+  childProcess2.on('error', (err) => {
+    console.error(`--- ERROR node server.js  ---\n${err.message}`);
+    console.log(err)
 
+  });
+}
+
+start()
+setTimeout(()=>{
+  console.log('[dev.js]: reset server');
+  childProcess2.kill();
+  start()
+}, 1000 * 10)
 
 // 监听中断信号（如 Ctrl+C）
 process.on('SIGINT', () => {
-    // 结束所有子进程
-    childProcess1.kill();
-    childProcess2.kill();
+  // 结束所有子进程
+  childProcess1.kill();
+  childProcess2.kill();
 
-    //process.kill(childProcess1.pid, 'SIGINT');
-    // process.kill(childProcess2.pid, 'SIGINT');
+  //process.kill(childProcess1.pid, 'SIGINT');
+  // process.kill(childProcess2.pid, 'SIGINT');
 
-    process.exit();
+  process.exit();
 });
