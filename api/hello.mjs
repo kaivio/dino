1fc1d5174cd18efa0c26235a7662ae0d38cf481a
kaivio <kaivio@github.com>
2023-09-06 15:20:47 +0800

    try fix error [2]

--- api/hello.mjs
+++ api/hello.mjs
@@ -1,9 +1,6 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+//import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse,
-) {
+export default function handler(request,response) {
   response.status(200).json({
     body: request.body,
     query: request.query,
